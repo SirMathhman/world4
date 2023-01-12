4673c96dfb863d3e2d9c47eeb9404d861891e0dd
SirMathhman <SirMathhman@gmail.com>
2023-01-12 14:48:57 -0500

    Introduce Evaluable

--- Formalization/src/lib.rs
+++ Formalization/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+trait Evaluable<T> {
+    fn evaluate(&self) -> T;
+
+    fn add(&self, other: Box<dyn Evaluable<T>>) -> Box<dyn Evaluable<T>>;
+}
+
+impl Evaluable<f64> for f64 {
+    fn evaluate(&self) -> f64 {
+        return *self;
+    }
+
+    fn add(&self, other: Box<dyn Evaluable<f64>>) -> Box<dyn Evaluable<f64>> {
+        return Box::new(self + other.evaluate());
+    }
 }
 
 #[cfg(test)]
-mod tests {
+mod evaluable_f64 {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn evaluate() {
+        assert_eq!(4f64, 4f64.evaluate());
+    }
+
+    #[test]
+    fn add(){
+        assert_eq!(20f64, 10f64.add(Box::new(10f64)).evaluate());
     }
 }
